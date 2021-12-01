487dd72ca34f6837303105ce4b2689660ff18839
ffahad30 <ff22@njit.edu>
2021-12-01 01:57:33 -0500

    only explanations for OOP principles article

--- tests/division_tests.py
+++ tests/division_tests.py
@@ -11,8 +11,6 @@ from calc.calculator import Calculator
 def test_calculator_divide_static():
     """testing that the calculator has a static method for division"""
     # pylint: disable=unused-argument,redefined-outer-name
-    # Act
-    # Assert
     assert Calculator.divide_numbers(6, 2) == 3
     assert Calculator.divide_numbers(10, 5) == 2
     assert Calculator.divide_numbers(30, 6) == 5
@@ -21,6 +19,5 @@ def test_calculator_divide_static():
     assert Calculator.history_calculations_count() == 5
     assert Calculator.first_calculation_result_in_history() == 3
     assert Calculator.last_calculation_result_in_history() == 3
-    # "float(float(Calculator.last_calculation_result_in_history())" is a type cast
     assert float(Calculator.last_calculation_result_in_history()) == 3.0
     pprint(Calculator.history)
